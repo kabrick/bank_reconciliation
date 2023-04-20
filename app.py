4b12c7e8b6b4879e611a9f75ba0cccc81009141f
Kabricks <kabuyedouglas53@gmail.com>
2023-04-20 18:06:30 +0300

    complete workflow

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, jsonify
+import datetime
+import time
+import json
+import os
+from flask import Flask, render_template, jsonify, request, url_for, redirect
 from functions import generators
-import json, time, random, datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     current_year = datetime.datetime.now().year
     return render_template('index.html', current_year=current_year)
@@ -22,15 +26,16 @@ def import_records():
 def complete_reconciliation():
     current_year = datetime.datetime.now().year
 
-    with open('banking_records.json', 'r') as f:
+    with open('json_files/banking_records.json', 'r') as f:
         # read the list from the file in JSON format
         banking_records = json.load(f)
 
-    with open('similar_system_records.json', 'r') as f:
+    with open('json_files/similar_system_records.json', 'r') as f:
         # read the list from the file in JSON format
         system_records = json.load(f)
 
-    return render_template('complete_reconciliation.html', banking_records=banking_records[:15], current_year=current_year,
+    return render_template('complete_reconciliation.html', banking_records=banking_records[:15],
+                           current_year=current_year,
                            system_records=system_records[:15])
 
 
@@ -45,14 +50,73 @@ def print_test():
     return "This is a print test"
 
 
-@app.route('/long_task')
-def long_task():
-    # Simulate a long task by sleeping for 10 seconds
-    time.sleep(10)
+@app.route('/run_reconciliation')
+def run_reconciliation():
+    with open('json_files/banking_records.json', 'r') as f:
+        # read the list from the file in JSON format
+        banking_records = json.load(f)
 
-    # Return 1 or 0 randomly
-    result = random.randint(0, 1)
-    return jsonify(result=result)
+    with open('json_files/similar_system_records_missing.json', 'r') as f:
+        # read the list from the file in JSON format
+        system_records = json.load(f)
+
+    bank_set = {
+        (record['memo'], record['credit_amount'], record['debit_amount'], record['balance_after'], record['timestamp'])
+        for record in banking_records}
+
+    system_set = {
+        (record['memo'], record['credit_amount'], record['debit_amount'], record['balance_after'], record['timestamp'])
+        for record in system_records}
+
+    unmatched_bank = [dict(zip(['memo', 'credit_amount', 'debit_amount', 'balance_after', 'timestamp'], record)) for
+                      record in bank_set - system_set]
+    unmatched_system = [dict(zip(['memo', 'credit_amount', 'debit_amount', 'balance_after', 'timestamp'], record)) for
+                        record in system_set - bank_set]
+
+    save_reconciliation = [{"unmatched_bank": unmatched_bank, "unmatched_system": unmatched_system}]
+
+    reconciliation_id = str(int(datetime.datetime.now().timestamp()))
+
+    file_path = 'json_files/reports/' + reconciliation_id + '.json'
+
+    # create the directory if it doesn't exist
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    with open(file_path, 'w') as f:
+        # write the list to the file in JSON format
+        json.dump(save_reconciliation, f)
+
+    bank_closing_balance = banking_records[-1]['balance_after']
+    system_closing_balance = system_records[-1]['balance_after']
+    unmatched_bank_len = len(unmatched_bank)
+    unmatched_system_len = len(unmatched_system)
+
+    time.sleep(5)
+
+    return {'reconciliation_id': reconciliation_id, 'bank_closing_balance': bank_closing_balance,
+            'unmatched_bank_len': unmatched_bank_len,
+            'system_closing_balance': system_closing_balance, 'unmatched_system_len': unmatched_system_len}
+
+
+@app.route('/submit_reconciliation', methods=['POST'])
+def submit_reconciliation():
+    reconciliation = {"date": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                      "bank_closing_balance": request.form['bank_closing_balance'],
+                      "system_closing_balance": request.form['system_closing_balance'],
+                      "balance_difference": request.form['balance_difference'],
+                      "performed_by": request.form['performed_by'], "bank_name": request.form['bank_name'],
+                      "memo": request.form['memo'], "reconciliation_id": request.form['reconciliation_id']}
+
+    file_path = 'json_files/reports.json'
+
+    # create the directory if it doesn't exist
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    with open(file_path, 'w') as f:
+        # write the list to the file in JSON format
+        json.dump(reconciliation, f)
+
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
--- functions/generators.py
+++ functions/generators.py
@@ -5,14 +5,17 @@ import random
 import datetime
 
 
+def get_memo():
+    return random.choice(["Payment", "Withdrawal", "Deposit", "Transfer"])
+
+
 def generate_banking_records():
-    memo = ["Payment", "Withdrawal", "Deposit", "Transfer"]
     records = []
     balance = 0
     timestamp = datetime.datetime(2020, 1, 1)
 
     for i in range(10000):
-        record = {'memo': random.choice(memo)}
+        record = {'memo': get_memo()}
 
         if random.choice([True, False]) or balance < 1:
             record['credit_amount'] = round(random.uniform(10000, 10000000))
@@ -28,11 +31,37 @@ def generate_banking_records():
         timestamp += datetime.timedelta(seconds=random.randint(1800, 10800))
         records.append(record)
 
-    with open('banking_records.json', 'w') as f:
+    with open('json_files/banking_records.json', 'w') as f:
         # write the list to the file in JSON format
         json.dump(records, f)
 
-    with open('similar_system_records.json', 'w') as f:
+    with open('json_files/similar_system_records.json', 'w') as f:
+        # write the list to the file in JSON format
+        json.dump(records, f)
+
+    # add a few more records to be fake
+    for i in range(12):
+        record = {'memo': get_memo()}
+
+        if random.choice([True, False]) or balance < 1:
+            record['credit_amount'] = round(random.uniform(10000, 10000000))
+            record['debit_amount'] = 0
+            balance += record['credit_amount']
+        else:
+            record['credit_amount'] = 0
+            record['debit_amount'] = round(random.uniform(10000, 10000000))
+            balance -= record['debit_amount']
+
+        record['balance_after'] = round(balance)
+        record['timestamp'] = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        timestamp += datetime.timedelta(seconds=random.randint(1800, 10800))
+        records.append(record)
+
+    with open('json_files/banking_records_missing.json', 'w') as f:
+        # write the list to the file in JSON format
+        json.dump(records, f)
+
+    with open('json_files/similar_system_records_missing.json', 'w') as f:
         # write the list to the file in JSON format
         json.dump(records, f)
 
